0a4d3a14457289315e1cd7cc2e2a1fda16af5dbb
rikxsa <sakuramaru908@gmail.comgit config --global user.email sakuramaru908@gmail.com>
2015-06-16 19:27:53 +0900

    Initial Commit

---
+++ JAM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JAM
+//
+//  Created by 桜木力丸 on 2015/06/16.
+//  Copyright (c) 2015年 桜木力丸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
